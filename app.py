095dfe3e993a03ca915aada658e04a77ccbb7eeb
zerosibe <kazehare.t@gmail.com>
2025-10-22 16:37:08 +0100

    ADD: app.py completed

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from lib.database_connection import DatabaseConnection
-from lib.artist_repository import ArtistRepository
+from lib.book_repository import BookRepository
+from lib.book import Book
 
 
 # Connect to the database
@@ -10,9 +11,30 @@ connection.connect()
 connection.seed("seeds/book_store.sql")
 
 # Retrieve all artists
-# artist_repository = ArtistRepository(connection)
-# artists = artist_repository.all()
+book_repository = BookRepository(connection)
+books = book_repository.all()
 
-# # List them out
-# for artist in artists:
-#     print(artist)
+# List them out
+for book in books:
+    print(book)
+
+# Find book
+print(book_repository.find(2))
+
+# Create new book
+book_repository.create(Book(None, 'Atomic Habits', 'James Clear'))
+books = book_repository.all()
+
+# List them out
+for book in books:
+    print(book)
+
+# Update book record
+book_repository.update(1, 'Animal Farm')
+print(book_repository.find(1))
+
+# Remove book record
+book_repository.delete(4)
+print(book_repository.all())
+
+
